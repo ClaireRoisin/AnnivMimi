361b534945145520964b952e2d31f28591e6b6b8
ClaireRoisin <claire.maurin@yahoo.fr>
2025-02-14 17:04:26 +0100

    Add files via upload

--- 70ansdeMaman.py
+++ 70ansdeMaman.py
@@ -45,9 +45,7 @@ with st.sidebar :
 # On indique au programme quoi faire en fonction du choix
 if selection == "Accueil":
     st.title("Genviève fête ses 70 ans !")
-    col1, col2, col3 = st.columns([1, 2, 1])  # La colonne du milieu est plus grande    
-    with col2:
-        st.image("mon_image.jpg", width=300)
+    st.image("mon_image.jpg", width=600)
 
     st.image("mimi1.jpg",width=300)
     st.header('Reservez dès maintenant votre Weekend du 6 et 7 avril 2025 !')
