3290f2e1a1a162a7adb161afd696d6f439cc7c03
ClaireRoisin <claire.maurin@yahoo.fr>
2025-02-14 17:16:22 +0100

    Add files via upload

--- 70ansdeMaman.py
+++ 70ansdeMaman.py
@@ -78,7 +78,7 @@ elif selection == "Organisation" :
     st.header('Pour dormir :')
     st.write('Les chambres sont composées de 2 ou 4 lits. Nous vous indiquerons votre chambre à votre arrivée !')
     st.write('Des draps peuvent être loués sur place pour ... € ou vous pouvez apporter les votres.')
-    col1, col2, col3 = st.columns([2, 2, 1])  # La colonne du milieu est plus grande
+    col1, col2, col3 = st.columns([1, 1, 1])  # La colonne du milieu est plus grande
     with col2:
         st.image("domino5.jpg",width=400)
     st.write('')
@@ -86,7 +86,7 @@ elif selection == "Organisation" :
     st.write('Le samedi midi : Comme chacun arrivera à son rythme, nous vous proposons que chacun apporte son pique-nique !')
     st.write("Le samedi soir : On s'occupe de tout 😉")
     st.write("Dimanche midi : Repas en mode 'Auberge espagnole'. Chacun apporte sa p'tite spécialité à partager (ne prévoyez pas trop, l'idée est de rentrer à vide !)")
-    col1, col2, col3 = st.columns([2, 2, 1])  # La colonne du milieu est plus grande
+    col1, col2, col3 = st.columns([1, 1, 1])  # La colonne du milieu est plus grande
     with col2:
         st.image("domino6.jpg",width=400)
 
