1612906dced16cb94ce7d09bc566bd11afd39a0b
ClaireRoisin <claire.maurin@yahoo.fr>
2025-02-14 17:10:11 +0100

    Add files via upload

--- 70ansdeMaman.py
+++ 70ansdeMaman.py
@@ -48,7 +48,7 @@ if selection == "Accueil":
     col1, col2, col3 = st.columns([1, 2, 1])  # La colonne du milieu est plus grande
     with col2:
         st.image("mimi1.jpg",width=700)
-    st.header('Reservez dès maintenant votre Weekend du 6 et 7 avril 2025 !')
+    st.header('Réservez dès maintenant votre Weekend du 6 et 7 avril 2025 !')
     
 elif selection == "Où ?":
     st.header("Sur l'île d'Oléron")
@@ -56,8 +56,9 @@ elif selection == "Où ?":
     point = [45.972916, -1.379777]              # coordo du site
     m = folium.Map(location=point,zoom_start=7)             #carte centrée sur le lieu
     folium.Marker(location=point,popup="L'emplacement idéal pour une fête de folie !").add_to(m)        # marker pour le lieu
-
-    st_folium(m, width=700, height=500) # affichage de la carte
+    col1, col2, col3 = st.columns([1, 2, 1])  # La colonne du milieu est plus grande
+    with col2:
+        st_folium(m, width=700, height=500) # affichage de la carte
 
     st.write("111-25 Rue Aristide Briand")
     st.write("17190 Saint-Georges-d'Oléron")
